4f6350a689e30904eaa0baac605167661ee910ea
janhbnr <janhuebner4@gmail.com>
2025-03-12 20:29:28 +0100

    feat: update billing cycle display in checkout price container
    Added logic to display the billing cycle as 'monthly' or 'yearly' based on the checkout data. Updated the recurring total display to reflect the billing cycle dynamically.

--- src/components/checkout/checkout-price-container.tsx
+++ src/components/checkout/checkout-price-container.tsx
@@ -9,13 +9,17 @@ interface Props {
 
 export function CheckoutPriceContainer({ checkoutData }: Props) {
   const recurringTotal = checkoutData?.recurring_totals?.total;
+  const billingCycle = checkoutData?.items[0]?.billing_cycle?.interval;
+
+  const billingCycleText = billingCycle === 'month' ? 'monthly' : billingCycle === 'year' ? 'yearly' : billingCycle;
+
   return (
     <>
       <div className={'text-base leading-[20px] font-semibold'}>Order summary</div>
       <CheckoutPriceAmount checkoutData={checkoutData} />
       {recurringTotal !== undefined ? (
         <div className={'pt-4 text-base leading-[20px] font-medium text-muted-foreground'}>
-          then {formatMoney(checkoutData?.recurring_totals?.total, checkoutData?.currency_code)} monthly
+          then {formatMoney(recurringTotal, checkoutData?.currency_code)} {billingCycleText}
         </div>
       ) : (
         <Skeleton className="mt-4 h-[20px] w-full bg-border" />
